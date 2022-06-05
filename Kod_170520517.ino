a6fcd22885edefa754589bf207a8aea58659778b
EnesOzkan <67148688+EnesSuleyman@users.noreply.github.com>
2022-06-05 22:57:12 +0300

    Update Kod_170520517.ino

--- Kod_170520517.ino
+++ Kod_170520517.ino
@@ -6,7 +6,7 @@ long sure, mesafe;
 
 void setup() {
 Serial.begin(9600); 
-motor.attach(10);
+motor.attach(10); //Motorun PWM sinyalini almış olduğu pin
 pinMode(trig, OUTPUT);
 pinMode(echo, INPUT);
 }
@@ -21,7 +21,7 @@ sure=pulseIn(echo,HIGH);
 mesafe=sure/58.2;
 Serial.println(mesafe);
   if (mesafe <20 ) {
-   motor.write(90);
+   motor.write(90);  //Motorumuz 90 derece dönecektir
    }
   else {
    motor.write(0);
